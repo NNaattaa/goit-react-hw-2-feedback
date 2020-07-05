b3bb0a175e20f60093e7b36951eec2a30bc5226c
NNaattaa <50961364+NNaattaa@users.noreply.github.com>
2020-07-05 12:19:28 +0300

    Updates

---
+++ precache-manifest.1dd74570779735a7292eadbe05d5b2cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7fdad25daec17433ad0e42d900e1d02",
+    "url": "/goit-react-hw-2-feedback/index.html"
+  },
+  {
+    "revision": "2aad546d3c97992d284e",
+    "url": "/goit-react-hw-2-feedback/static/css/main.2526452b.chunk.css"
+  },
+  {
+    "revision": "9ed4387240011b1a2919",
+    "url": "/goit-react-hw-2-feedback/static/js/2.4fde017a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/goit-react-hw-2-feedback/static/js/2.4fde017a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2aad546d3c97992d284e",
+    "url": "/goit-react-hw-2-feedback/static/js/main.4375a639.chunk.js"
+  },
+  {
+    "revision": "584a37f24669430755ff",
+    "url": "/goit-react-hw-2-feedback/static/js/runtime-main.e681874b.js"
+  }
+]);
